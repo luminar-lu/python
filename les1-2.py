1a3c4bca150abd99ace5d45d9da1388ae05cc85a
luminar-lu <74783237+luminar-lu@users.noreply.github.com>
2020-11-20 21:16:01 +0300

    Delete les1-2.py

--- les1-2.py
+++
@@ -1,7 +0,0 @@
-print("Введите время в секундах:")
-num = int(input())
-hours = num // 3600
-minutes = num % 3600 // 60
-seconds = num % 60
-print(f"Время  в формате ЧЧ:ММ:СС : {hours}:{minutes}:{seconds}")
-
